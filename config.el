e37a723350c4730289efd04329fc7a16b3712f9b
redbeardy mcgee <redbeardymcgee@gmail.com>
2021-10-25 11:01:38 -0500

    Switch to parinfer instead of lispy

--- config.el
+++ config.el
@@ -1,16 +1,4 @@
 (setq user-full-name "redbeardy mcgee"
       user-mail-address "redbeardymcgee@gmail.com")
 
-(lispyville-set-key-theme '(operators
-                            c-w
-                            c-u
-                            prettify
-                            (atom-movement t)
-                            additional-movement
-                            commentary
-                            slurp/barf-lispy
-                            wrap
-                            additional
-                            additional-insert
-                            arrows
-                            escape))
+(after! parinfer (setq parinfer-rust-check-before-enable nil))
